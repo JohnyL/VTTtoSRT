f85a2ce7987d2a26cf642dd370c6d4d83f33a9a7
Eugene <sektorLP@yandex.ru>
2017-09-01 17:57:18 +0300

    Add files via upload

---
+++ source/Root.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace VTTtoSRT
+{
+	partial class Root
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnConvert = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btnConvert
+			// 
+			this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.btnConvert.Location = new System.Drawing.Point(12, 12);
+			this.btnConvert.Name = "btnConvert";
+			this.btnConvert.Size = new System.Drawing.Size(260, 33);
+			this.btnConvert.TabIndex = 0;
+			this.btnConvert.Text = "Convert VTT to SRT";
+			this.btnConvert.UseVisualStyleBackColor = true;
+			this.btnConvert.Click += new System.EventHandler(this.OnConvert);
+			// 
+			// Root
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(285, 52);
+			this.Controls.Add(this.btnConvert);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "Root";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "VTT to SRT";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnConvert;
+	}
+}
+
---
+++ source/Root.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace VTTtoSRT
+{
+	public partial class Root : Form
+	{
+
+		public Root() => InitializeComponent();
+
+		private async void OnConvert(object sender, EventArgs e)
+		{
+			var dialog = new OpenFileDialog
+			{
+				Filter = "VTT files|*.vtt",
+				RestoreDirectory = true,
+				Multiselect = true
+			};
+
+			if (dialog.ShowDialog() != DialogResult.OK) return;
+
+			string time_line = null, new_line = null;
+			string pattern = @"\d{2}:\d{2}:\d{2}\.\d{1,3}\s-->\s\d{2}:\d{2}:\d{2}\.\d{1,3}";
+			Match match = null;
+			var regex = new Regex(pattern);
+
+			foreach (var vttFile in dialog.FileNames)
+			{
+
+				int i = 0, x = 0;
+				var dest_file = Path.Combine(Path.GetDirectoryName(vttFile), $"{Path.GetFileNameWithoutExtension(vttFile)}.srt");
+				using (var srt = new StreamWriter(dest_file))
+				{
+					var lines = File.ReadLines(vttFile);
+					foreach (string line in lines)
+					{
+						++i;
+						match = regex.Match(line);
+						if (match.Success)
+						{
+							++x;
+							time_line = match.Value.Replace(".", ",");
+							new_line = x == 1 ? "" : "\r\n";
+							await srt.WriteLineAsync($"{new_line}{x}\r\n{time_line}");
+						}
+						else if (line.Length > 0)
+						{
+							if (i == 1) continue; //Skip first line WEBTTV
+							await srt.WriteLineAsync(line);
+						}
+					}
+
+					File.Delete(vttFile);
+				}
+
+			}
+			MessageBox.Show("Done!");
+		}
+	}
+}
