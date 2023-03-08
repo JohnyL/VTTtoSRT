7348ee907f39ce99bbf11d7f5ea84eb63cdb21e7
Johny <sektorLP@ya.ru>
2023-03-08 21:04:24 +0300

    1) Switched to .NET 8. 2) Got rid of "var". 3) Added checks for absence of hours in timeline (changed regex because of this). 4) Used file-scoped namespace. 5) Use new using of "using". 6) Added option not to delete file after conversion. 7) Added title to file selection dialog to make it clear that more than one file can be selected. 8) Made two lines out of one-liners.

--- source/Root.Designer.cs
+++ source/Root.Designer.cs
@@ -1,64 +1,79 @@
 ﻿namespace VTTtoSRT
 {
-	partial class Root
-	{
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.IContainer components = null;
+  partial class Root
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (components != null))
-			{
-				components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
 
-		#region Windows Form Designer generated code
+    #region Windows Form Designer generated code
 
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.btnConvert = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// btnConvert
-			// 
-			this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.btnConvert.Location = new System.Drawing.Point(12, 12);
-			this.btnConvert.Name = "btnConvert";
-			this.btnConvert.Size = new System.Drawing.Size(260, 33);
-			this.btnConvert.TabIndex = 0;
-			this.btnConvert.Text = "Convert VTT to SRT";
-			this.btnConvert.UseVisualStyleBackColor = true;
-			this.btnConvert.Click += new System.EventHandler(this.OnConvert);
-			// 
-			// Root
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(285, 52);
-			this.Controls.Add(this.btnConvert);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-			this.Name = "Root";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "VTT to SRT";
-			this.ResumeLayout(false);
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      btnConvert = new Button();
+      chkDeleteSrtFile = new CheckBox();
+      SuspendLayout();
+      // 
+      // btnConvert
+      // 
+      btnConvert.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold, GraphicsUnit.Point);
+      btnConvert.Location = new Point(14, 14);
+      btnConvert.Margin = new Padding(4, 3, 4, 3);
+      btnConvert.Name = "btnConvert";
+      btnConvert.Size = new Size(303, 38);
+      btnConvert.TabIndex = 0;
+      btnConvert.Text = "Convert VTT to SRT";
+      btnConvert.UseVisualStyleBackColor = true;
+      btnConvert.Click += OnConvert;
+      // 
+      // chkDeleteSrtFile
+      // 
+      chkDeleteSrtFile.AutoSize = true;
+      chkDeleteSrtFile.Location = new Point(14, 58);
+      chkDeleteSrtFile.Name = "chkDeleteSrtFile";
+      chkDeleteSrtFile.Size = new Size(199, 19);
+      chkDeleteSrtFile.TabIndex = 1;
+      chkDeleteSrtFile.Text = "Delete SRT file(s) after converting";
+      chkDeleteSrtFile.UseVisualStyleBackColor = true;
+      // 
+      // Root
+      // 
+      AutoScaleDimensions = new SizeF(7F, 15F);
+      AutoScaleMode = AutoScaleMode.Font;
+      ClientSize = new Size(332, 87);
+      Controls.Add(chkDeleteSrtFile);
+      Controls.Add(btnConvert);
+      FormBorderStyle = FormBorderStyle.FixedSingle;
+      Margin = new Padding(4, 3, 4, 3);
+      Name = "Root";
+      StartPosition = FormStartPosition.CenterScreen;
+      Text = "VTT to SRT";
+      ResumeLayout(false);
+      PerformLayout();
+    }
 
-		}
+    #endregion
 
-		#endregion
-
-		private System.Windows.Forms.Button btnConvert;
-	}
+    private System.Windows.Forms.Button btnConvert;
+    private CheckBox chkDeleteSrtFile;
+  }
 }
 
--- source/Root.cs
+++ source/Root.cs
@@ -1,62 +1,59 @@
-﻿using System;
-using System.IO;
-using System.Text.RegularExpressions;
-using System.Windows.Forms;
+﻿namespace VTTtoSRT;
 
-namespace VTTtoSRT
+public partial class Root : Form
 {
-	public partial class Root : Form
-	{
+  public Root() => InitializeComponent();
 
-		public Root() => InitializeComponent();
+  private async void OnConvert(object sender, EventArgs e)
+  {
+    OpenFileDialog dialog = new()
+    {
+      Title = "Select one or more .SRT files",
+      Filter = "VTT files|*.vtt",
+      RestoreDirectory = true,
+      Multiselect = true
+    };
 
-		private async void OnConvert(object sender, EventArgs e)
-		{
-			var dialog = new OpenFileDialog
-			{
-				Filter = "VTT files|*.vtt",
-				RestoreDirectory = true,
-				Multiselect = true
-			};
+    if (dialog.ShowDialog() != DialogResult.OK)
+      return;
 
-			if (dialog.ShowDialog() != DialogResult.OK) return;
+    Match match;
+    string time_line, new_line;
+    Regex regex = new(@"(?'hoursStart'\d{2}:)?(?'start'\d{2}:\d{2}\.\d{1,3})\s-->\s(?'hoursEnd'\d{2}:)?(?'end'\d{2}:\d{2}\.\d{1,3})");
+    bool to_delete_vtt = chkDeleteSrtFile.Checked;
 
-			string time_line = null, new_line = null;
-			string pattern = @"\d{2}:\d{2}:\d{2}\.\d{1,3}\s-->\s\d{2}:\d{2}:\d{2}\.\d{1,3}";
-			Match match = null;
-			var regex = new Regex(pattern);
+    foreach (string vttFile in dialog.FileNames)
+    {
+      int i = 0, x = 0;
+      string dest_file = Path.Combine(Path.GetDirectoryName(vttFile)!, $"{Path.GetFileNameWithoutExtension(vttFile)}.srt");
+      if (File.Exists(dest_file))
+        File.Delete(dest_file);
+      using StreamWriter srt = new(dest_file);
+      IEnumerable<string> lines = File.ReadLines(vttFile);
 
-			foreach (var vttFile in dialog.FileNames)
-			{
+      foreach (string line in lines)
+      {
+        match = regex.Match(line);
+        if (match.Success)
+        {
+          // Reconstruct timeline.
+          // If hours are absent, add them back as "00".
+          time_line = $"{(match.Groups["hoursStart"].Value == "" ? "00:" : "")}{match.Groups["start"].Value} --> {(match.Groups["hoursEnd"].Value == "" ? "00:" : "")}{match.Groups["end"].Value}";
+          time_line = time_line.Replace(".", ",");
+          new_line = ++x == 1 ? "" : "\r\n";
+          await srt.WriteLineAsync($"{new_line}{x}\r\n{time_line}");
+        }
+        else if (line.Length > 0)
+        {
+          if (++i == 1)
+            continue; //Skip first line WEBTTV
+          await srt.WriteLineAsync(line);
+        }
+      }
 
-				int i = 0, x = 0;
-				var dest_file = Path.Combine(Path.GetDirectoryName(vttFile), $"{Path.GetFileNameWithoutExtension(vttFile)}.srt");
-				using (var srt = new StreamWriter(dest_file))
-				{
-					var lines = File.ReadLines(vttFile);
-					foreach (string line in lines)
-					{
-						++i;
-						match = regex.Match(line);
-						if (match.Success)
-						{
-							++x;
-							time_line = match.Value.Replace(".", ",");
-							new_line = x == 1 ? "" : "\r\n";
-							await srt.WriteLineAsync($"{new_line}{x}\r\n{time_line}");
-						}
-						else if (line.Length > 0)
-						{
-							if (i == 1) continue; //Skip first line WEBTTV
-							await srt.WriteLineAsync(line);
-						}
-					}
-
-					File.Delete(vttFile);
-				}
-
-			}
-			MessageBox.Show("Done!");
-		}
-	}
+      if (to_delete_vtt)
+        File.Delete(vttFile);
+    }
+    MessageBox.Show("Done!", "VTT to SRT", MessageBoxButtons.OK, MessageBoxIcon.Information);
+  }
 }
